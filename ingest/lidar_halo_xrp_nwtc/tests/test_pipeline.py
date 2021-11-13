6690f9e9aa33eaeb5e124cb6ebe551fe63c56b6b
Maxwell Levin <maxwellevin@users.noreply.github.com>
2021-11-12 16:32:26 -0800

    Added Ingest: Lidar Halo XRP NWTC (#3)
    * Initial commit of lidar_halo_xrp_nwtc
    
    * Added Dan's code
    
    * Added mapping
    
    * Use better assertion method
    
    * Minor updates
    
    * Clean up code

---
+++ ingest/lidar_halo_xrp_nwtc/tests/test_pipeline.py
@@ -0,0 +1,23 @@
+import os
+import xarray as xr
+from utils import expand, set_dev_env
+from ingest.lidar_halo_xrp_nwtc import LidarHaloXrpPipeline
+
+parent = os.path.dirname(__file__)
+
+
+def test_pipeline_at_nwtc():
+    set_dev_env()
+    pipeline = LidarHaloXrpPipeline(
+        expand("config/pipeline_config_nwtc.yml", parent),
+        expand("config/storage_config.yml", parent),
+    )
+    output = pipeline.run(
+        expand("tests/data/input/nwtc/Stare_199_20210510_00.hpl", parent)
+    )
+    expected = xr.open_dataset(
+        expand(
+            "tests/data/expected/nwtc/nwtc.lidar-halo_xrp.b0.20210510.000125.nc", parent
+        )
+    )
+    xr.testing.assert_allclose(output, expected)

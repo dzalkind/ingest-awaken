c88fa34bc33df035314472dbc96b5e4e9c16f129
jmcvey3 <james13rmh@gmail.com>
2022-01-20 10:50:22 -0800

    lidar disdrometer ingest

---
+++ ingest/aml_ld_z01/pipeline/pipeline.py
@@ -0,0 +1,35 @@
+import os
+import cmocean
+import numpy as np
+import pandas as pd
+import xarray as xr
+import matplotlib.pyplot as plt
+
+from typing import Dict
+from tsdat import DSUtil
+from utils import A2ePipeline, format_time_xticks
+
+
+class Pipeline(A2ePipeline):
+    """--------------------------------------------------------------------------------
+    AML_LD_Z01 INGESTION PIPELINE
+
+    awaken lidar disdrometer ingest
+
+    --------------------------------------------------------------------------------"""
+
+    def hook_customize_raw_datasets(
+        self, raw_dataset_mapping: Dict[str, xr.Dataset]
+    ) -> Dict[str, xr.Dataset]:
+        return raw_dataset_mapping
+
+    def hook_customize_dataset(
+        self, dataset: xr.Dataset, raw_mapping: Dict[str, xr.Dataset]
+    ) -> xr.Dataset:
+        return dataset
+
+    def hook_finalize_dataset(self, dataset: xr.Dataset) -> xr.Dataset:
+        return dataset
+
+    def hook_generate_and_persist_plots(self, dataset: xr.Dataset):
+        return
---
+++ ingest/aml_ld_z01/tests/test_pipeline.py
@@ -0,0 +1,18 @@
+import os
+import xarray as xr
+from utils import expand, set_dev_env
+from ingest.aml_ld_z01 import Pipeline
+
+parent = os.path.dirname(__file__)
+
+
+# TODO – Developer: Update paths to your input files here.
+def test_pipeline_at_awaken():
+    set_dev_env()
+    pipeline = Pipeline(
+        expand("config/pipeline_config_awaken.yml", parent),
+        expand("config/storage_config.yml", parent),
+    )
+    output = pipeline.run(expand("tests/data/input/data.csv", parent))
+    expected = xr.open_dataset(expand("tests/data/expected/data.csv", parent))
+    xr.testing.assert_allclose(output, expected)

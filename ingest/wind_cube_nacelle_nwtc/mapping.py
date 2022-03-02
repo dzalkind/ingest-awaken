e3af7e1eed6e05577e806e89037e4b5f201bd651
dzalkind <dzalkind@nrel.gov>
2022-03-02 12:16:49 -0700

    Add processed file mapping

--- ingest/wind_cube_nacelle_nwtc/mapping.py
+++ ingest/wind_cube_nacelle_nwtc/mapping.py
@@ -12,23 +12,23 @@ from . import Pipeline
 # full filepath will be passed to the compiled regex pattern, so you can optionally
 # match the directory structure in addition to (or instead of) the file basename.
 mapping: Dict["AnyStr@compile", IngestSpec] = {
-    
     # Real-time data
     # Mapping for Raw Data -> Ingest
-    re.compile(r".*_real_time_data_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}\.csv"): IngestSpec(
+    re.compile(
+        r".*_real_time_data_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}\.csv"
+    ): IngestSpec(
         pipeline=Pipeline,
         pipeline_config=expand("config/pipeline_config_rtd.yml", __file__),
         storage_config=expand("config/storage_config.yml", __file__),
         name="wind_cube_nacelle_rtd",
     ),
     # Mapping for Processed Data -> Ingest (so we can reprocess plots)
-    re.compile(r"YOUR-REGEX-HERE"): IngestSpec(
+    re.compile(r".*nwtc\.wind_cube_nacelle_rtd\.b0.\d{8}.\d{6}.nc"): IngestSpec(
         pipeline=Pipeline,
         pipeline_config=expand("config/pipeline_config_rtd.yml", __file__),
         storage_config=expand("config/storage_config.yml", __file__),
         name="plot_wind_cube_nacelle_nwtc",
     ),
-    
     ## 10-min avg data
     # Mapping for Raw Data -> Ingest
     re.compile(r".*_average_data_\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}\.csv"): IngestSpec(
@@ -37,9 +37,8 @@ mapping: Dict["AnyStr@compile", IngestSpec] = {
         storage_config=expand("config/storage_config.yml", __file__),
         name="wind_cube_nacelle_rtd",
     ),
-    # Mapping for Processed Data -> Ingest (so we can reprocess plots)  
-    # TODO
-    re.compile(r"YOUR-REGEX-HERE"): IngestSpec(
+    # Mapping for Processed Data -> Ingest (so we can reprocess plots)
+    re.compile(r".*nwtc\.wind_cube_nacelle_sta\.b0.\d{8}.\d{6}.nc"): IngestSpec(
         pipeline=Pipeline,
         pipeline_config=expand("config/pipeline_config_rtd.yml", __file__),
         storage_config=expand("config/storage_config.yml", __file__),

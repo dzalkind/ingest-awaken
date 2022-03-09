9f782ed97b88ab19fe9bf5185fe941cc1ea5c43d
dzalkind <dzalkind@nrel.gov>
2022-03-09 14:38:32 -0700

    Update test paths

--- ingest/wind_cube_nwtc/tests/test_pipeline.py
+++ ingest/wind_cube_nwtc/tests/test_pipeline.py
@@ -9,10 +9,33 @@ parent = os.path.dirname(__file__)
 # TODO – Developer: Update paths to your input files here.
 def test_pipeline_at_nwtc():
     set_dev_env()
+
+    # Real-time data
     pipeline = Pipeline(
-        expand("config/pipeline_config_nwtc.yml", parent),
+        expand("config/pipeline_config_nwtc_rtd.yml", parent),
         expand("config/storage_config.yml", parent),
     )
-    output = pipeline.run(expand("tests/data/input/data.csv", parent))
-    expected = xr.open_dataset(expand("tests/data/expected/data.csv", parent))
+    output = pipeline.run(
+        expand("tests/data/input/WLS71460_2022_02_01__00_00_00.rtd.7z", parent)
+    )
+    expected = xr.open_dataset(
+        expand(
+            "tests/data/expected/nwtc.wind_cube_nwtc_rtd.b0.20220201.000000.nc", parent
+        )
+    )
+    xr.testing.assert_allclose(output, expected)
+
+    # 10-minute statistics
+    pipeline = Pipeline(
+        expand("config/pipeline_config_nwtc_sta.yml", parent),
+        expand("config/storage_config.yml", parent),
+    )
+    output = pipeline.run(
+        expand("tests/data/input/WLS71460_2022_02_02__00_01_00.sta.7z", parent)
+    )
+    expected = xr.open_dataset(
+        expand(
+            "tests/data/expected/nwtc.wind_cube_nwtc_sta.b0.20220202.000100.nc", parent
+        )
+    )
     xr.testing.assert_allclose(output, expected)

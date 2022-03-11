b927b454fe240cdf75f432d7da9ccf0af0090d80
dzalkind <dzalkind@nrel.gov>
2022-03-11 14:19:30 -0700

    Update test data with new Halo info

--- ingest/lidar_halo_xrp_nwtc/mapping.py
+++ ingest/lidar_halo_xrp_nwtc/mapping.py
@@ -20,7 +20,7 @@ mapping: Dict["AnyStr@compile", IngestSpec] = {
         name="awaken_halo_ingest",
     ),
     # Mapping for Processed Data -> Ingest (so we can reprocess plots)
-    re.compile(r".*nwtc\.lidar-halo_xrp\.b0\.\d{8}\.\d{6}\.nc"): IngestSpec(
+    re.compile(r".*nwtc\.lidar-halo_xrp.*\.b0\.\d{8}\.\d{6}\.nc"): IngestSpec(
         pipeline=LidarHaloXrpPipeline,
         pipeline_config=expand("config/pipeline_config_nwtc.yml", __file__),
         storage_config=expand("config/storage_config.yml", __file__),

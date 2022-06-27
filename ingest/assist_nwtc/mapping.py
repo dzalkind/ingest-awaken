0b1447477b6a7442b96b5dc0f14d1b51f808af23
dzalkind <dzalkind@nrel.gov>
2022-06-27 13:46:38 -0600

    Add mapping and NREL server storage

--- ingest/assist_nwtc/mapping.py
+++ ingest/assist_nwtc/mapping.py
@@ -9,12 +9,31 @@ from . import Pipeline
 # full filepath will be passed to the compiled regex pattern, so you can optionally
 # match the directory structure in addition to (or instead of) the file basename.
 mapping: Dict["AnyStr@compile", IngestSpec] = {
-    # Mapping for Raw Data -> Ingest
-    re.compile(r".*assistsummary.*.cdf"): IngestSpec(
+    # Mapping for Raw Data -> Ingest (DAP)
+    re.compile(r".*assist\.z\d{2}.00.\d{8}\.\d{6}\.assistsummary\.cdf"): IngestSpec(
         pipeline=Pipeline,
         pipeline_config=expand("config/pipeline_config_nwtc.yml", __file__),
         storage_config=expand("config/storage_config.yml", __file__),
-        name="assist_summary",
+        name="assist_summary_DAP",
+    ),   
+    # Mapping for Raw Data -> Ingest (NREL Server)
+    re.compile(r".*Assist-10.*assistsummary.*.cdf"): IngestSpec(
+        pipeline=Pipeline,
+        pipeline_config=expand("config/pipeline_config_nwtc.yml", __file__),
+        storage_config=expand("config/storage_config_10.yml", __file__),
+        name="assist_summary_10",
+    ),
+    re.compile(r".*Assist-11.*assistsummary.*.cdf"): IngestSpec(
+        pipeline=Pipeline,
+        pipeline_config=expand("config/pipeline_config_nwtc.yml", __file__),
+        storage_config=expand("config/storage_config_11.yml", __file__),
+        name="assist_summary_11",
+    ),
+    re.compile(r".*Assist-12.*assistsummary.*.cdf"): IngestSpec(
+        pipeline=Pipeline,
+        pipeline_config=expand("config/pipeline_config_nwtc.yml", __file__),
+        storage_config=expand("config/storage_config_12.yml", __file__),
+        name="assist_summary_12",
     ),
     # Mapping for Processed Data -> Ingest (so we can reprocess plots)
     re.compile(r"YOUR-REGEX-HERE"): IngestSpec(

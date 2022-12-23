90d695b3552a6f1282b61f4c1b71af2faa877cbc
dzalkind <dzalkind@nrel.gov>
2022-12-23 12:51:47 -0700

    Update locations, clean up non-existent sites, update tests

--- pipelines/met_campbell/test/test_pipeline.py
+++ pipelines/met_campbell/test/test_pipeline.py
@@ -4,15 +4,15 @@ from tsdat import PipelineConfig, assert_close
 
 
 def test_met_campbell_pipeline():
-    config_path = Path("pipelines/met_campbell/config/pipeline_sa4.yaml")
+    config_path = Path("pipelines/met_campbell/config/pipeline_sa1.yaml")
     config = PipelineConfig.from_yaml(config_path)
     pipeline = config.instantiate_pipeline()
 
     test_file = (
-        "pipelines/met_campbell/test/data/input/sa4.met.z01.00.20211207.130000.txt"
+        "pipelines/met_campbell/test/data/input/sa1.met.z01.00.20211207.130000.txt"
     )
     expected_file = (
-        "pipelines/met_campbell/test/data/expected/sa4.met_z01.a1.20211207.130000.nc"
+        "pipelines/met_campbell/test/data/expected/sa1.met_z01.a1.20211207.130000.nc"
     )
 
     dataset = pipeline.run([test_file])
